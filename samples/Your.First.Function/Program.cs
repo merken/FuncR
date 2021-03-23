7c0f58fe83a93c19958c20a3a3b6f28cb887f619
merken <merken.maarten@gmail.com>
2021-03-23 23:20:24 +0100

    Align code to docs

--- samples/Your.First.Function/Program.cs
+++ samples/Your.First.Function/Program.cs
@@ -4,14 +4,19 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Your.First.Function
 {
+    public interface IServiceWithoutImplementation
+    {
+        string Foo(string bar);
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var services = new ServiceCollection();
             services.AddScopedFunction<IServiceWithoutImplementation>
-                // Implements                  string Foo(Bar bar)
-                (nameof(IServiceWithoutImplementation.Foo)).Runs<Bar, string>(bar =>
+                // Implements                  string Foo(string bar)
+                (nameof(IServiceWithoutImplementation.Foo)).Runs<string, string>(bar =>
                 {
                     return $"From func '{bar}'";
                 });
@@ -19,8 +24,8 @@ namespace Your.First.Function
             var myServiceWithoutImplementation =
                 services.BuildServiceProvider().GetRequiredService<IServiceWithoutImplementation>();
 
-            // Prints: "From func 'BAR'"
-            Console.WriteLine(myServiceWithoutImplementation.Foo(new Bar()));
+            // Prints: "From func 'Bar'"
+            Console.WriteLine(myServiceWithoutImplementation.Foo("Bar"));
         }
     }
 }
